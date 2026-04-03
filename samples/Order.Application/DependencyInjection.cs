abeca467e0bbefc350c0fb399b2da90a8f5197cd
uitanmaciel <uitan.s.maciel@gmail.com>
2026-04-03 19:01:55 -0300

    :sparkles: feat: Updates the application layer of the example project

--- samples/Order.Application/DependencyInjection.cs
+++ samples/Order.Application/DependencyInjection.cs
@@ -1,11 +1,6 @@
 using DevSource.Dispatcher.Engine;
 using DevSource.Dispatcher.Generated;
 using Microsoft.Extensions.DependencyInjection;
-using Order.Application.Behaviors;
-using Order.Application.Commands;
-using Order.Application.Handlers;
-using Order.Application.Notifications;
-using Order.Application.Queries;
 
 namespace Order.Application;
 
@@ -13,11 +8,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddOrderApplication(this IServiceCollection services)
     {
-        services.AddTransient<DevSource.Dispatcher.Commands.ICommandHandler<CreateOrderCommand, Guid>, CreateOrderHandler>();
-        services.AddTransient<DevSource.Dispatcher.Queries.IQueryHandler<GetOrderByIdQuery, Dtos.OrderDto?>, GetOrderByIdHandler>();
-        services.AddTransient<DevSource.Dispatcher.Notifications.INotificationHandler<OrderCreatedNotification>, OrderCreatedHandler>();
-        services.AddTransient(typeof(DevSource.Dispatcher.IPipelineBehavior<,>), typeof(CorrelationLoggingBehavior<,>));
-        services.AddDispatcher<GeneratedDispatcher>();
-        return services;
+        return services.AddGeneratedDispatcher();
     }
 }
